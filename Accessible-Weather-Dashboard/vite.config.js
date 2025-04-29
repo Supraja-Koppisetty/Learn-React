32ccef6d1c0820efb18f41ae7012f0ad2dc33632
Supraja-Koppisetty <kvsupraja2710@gmail.com>
2025-04-29 16:44:27 -0400

    Deploy Vite app manually to GitHub Pages

--- Accessible-Weather-Dashboard/vite.config.js
+++ Accessible-Weather-Dashboard/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/accessible-weather-dashboard/",
+  base: "/Learn-React/Accessible-Weather-Dashboard/",
   plugins: [react()],
 });
